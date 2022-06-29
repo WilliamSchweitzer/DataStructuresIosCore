7d1d0df0820910e1a0472da399bd845e8055e881
weschweitzer@gmail.com <weschweitzer@gmail.com>
2022-06-29 09:05:17 -0400

    Initial Commit

---
+++ DataStructures/DataStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructuresApp.swift
+//  DataStructures
+//
+//  Created by Will Schweitzer on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct DataStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
